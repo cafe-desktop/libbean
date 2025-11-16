170907b717e3b8ae0114821892c7095cea2ded00
Pablo Barciela <scow@riseup.net>
2025-11-16 13:59:23 +0100

    ci: test lintian

---
+++ build-debs.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+set -v
+set -x
+
+# sample:
+# ./build-debs https://github.com/cafe-desktop/debian-packages master cafe-sensors-applet
+# or just ./build-debs for current repo with master branch
+
+if [ $# -eq 0 ]
+then
+  giturl=https://github.com/${OWNER_NAME}/debian-packages
+  gitbranch=master
+  gitrepo=${REPO_NAME}
+else
+  giturl=$1
+  gitbranch=$2
+  gitrepo=$3
+fi
+
+aptitude install -y devscripts dh-make dh-exec gdebi lintian
+cd ${START_DIR}
+mkdir -p html-report
+git clone --depth 1 ${giturl}.git -b ${gitbranch} tmp-debs
+cp -dpR ./tmp-debs/${gitrepo}/debian .
+tar cfJv ../debian.tar.xz debian
+mk-build-deps debian/control
+gdebi --n *.deb
+rm *deps*
+dpkg-buildpackage -b -rfakeroot -us -uc
+cd ..
+tar cfJv deb_packages.tar.xz *deb *buildinfo *changes
+if dpkg -i *.deb; then
+  echo
+else
+  aptitude -f -y install
+  dpkg -i *.deb
+fi
+lintian -i -EIL+pedantic *.changes
+mv *deb *buildinfo *changes debian.tar.xz deb_packages.tar.xz .${START_DIR}/html-report
