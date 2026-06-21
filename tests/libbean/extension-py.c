dc5becad2f77c1b56c04e8e912b6b83371cf6773
Pablo Barciela <scow@riseup.net>
2026-06-21 12:20:40 +0200

    extension-py: Fix test-extension-py

--- tests/libbean/extension-py.c
+++ tests/libbean/extension-py.c
@@ -59,7 +59,7 @@ test_extension_py_instance_refcount (BeanEngine     *engine,
 
   g_object_add_weak_pointer (extension, (gpointer *) &extension);
 
-  g_assert_cmpint (extension->ref_count, ==, 2);
+  g_assert_cmpint (extension->ref_count, ==, 1);
 
   g_object_unref (extension);
   g_assert (extension == NULL);
@@ -96,7 +96,7 @@ test_extension_py_activatable_subject_refcount (BeanEngine     *engine,
   wrapper = g_object_get_data (object, "PyGObject::wrapper");
   g_assert_cmpint (wrapper->ob_refcnt, ==, 1);
 
-  g_assert_cmpint (G_OBJECT (extension)->ref_count, ==, 2);
+  g_assert_cmpint (G_OBJECT (extension)->ref_count, ==, 1);
   g_object_unref (extension);
 
   /* We unreffed the extension, so it should have been destroyed and our dummy
